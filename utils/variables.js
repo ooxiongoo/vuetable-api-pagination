dade8930ea99c4ed29dca1f7743e80f6060eb988
ooxiongoo <53354088+ooxiongoo@users.noreply.github.com>
2019-09-11 10:43:32 -0700

    Add files via upload

---
+++ utils/variables.js
@@ -0,0 +1,7 @@
+var base_path = "http://localhost:8889/";
+
+export const API_PATH_MD_TABLE = base_path + "md_table_indx/";
+export const API_PATH_MD_ATTRIBUTES = base_path + "md_attributes_indx/";
+
+export const API_PATH_MD_TABLE_ALL = API_PATH_MD_TABLE + "all";
+export const API_PATH_MD_ATTRIBUTES_ALL = API_PATH_MD_ATTRIBUTES + "all";
